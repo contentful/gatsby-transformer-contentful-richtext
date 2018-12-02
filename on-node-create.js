c5395bbf781bfd60835b28bab0b8a27f2d92c054
JP Wright <jpwright31@gmail.com>
2018-12-02 11:04:27 +0100

    Add build for manual testing

---
+++ on-node-create.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const crypto = require(`crypto`);
+
+module.exports = async function onCreateNode({
+  node,
+  getNode,
+  loadNodeContent,
+  actions,
+  createNodeId,
+  reporter
+}, pluginOptions) {
+  const {
+    createNode,
+    createParentChildLink
+  } = actions; // We only care about markdown content.
+
+  if (node.internal.mediaType !== `text/richtext`) {
+    return;
+  }
+
+  const content = await loadNodeContent(node);
+  const richTextNode = {
+    id: createNodeId(`${node.id} >>> ContentfulRichText`),
+    children: [],
+    parent: node.id,
+    internal: {
+      content,
+      type: `ContentfulRichText`
+    }
+  };
+  richTextNode.internal.contentDigest = crypto.createHash(`md5`).update(JSON.stringify(richTextNode)).digest(`hex`);
+  createNode(richTextNode);
+  createParentChildLink({
+    parent: node,
+    child: richTextNode
+  });
+};
